41f366f66b031932f4a6ca12bd46e7b430ff6ab2
Mark Stacey <markjstacey@gmail.com>
2014-03-17 19:55:38 -0230

    Finish migrating to using hash rather than array values (apart from dependency queue)

--- hw2.pl
+++ hw2.pl
@@ -83,7 +83,7 @@ sub run_program {
     my $P_fn = shift;
     my $M_fn = shift;
     
-	my $machine = = parse_machine($M_fn);
+	my $machine = parse_machine($M_fn);
     
     my $d_in_use = 0;
     my $iu_in_use = 0;
@@ -104,7 +104,7 @@ sub run_program {
         # check for iu availability
         if ($iu_in_use eq 0 && scalar @instr_queue > 0) {
             my $instr = $instr_queue[0];
-            my $operator = $$instr[1];
+            my $operator = $instr->{'Op'};
 
             my $d_time;
             if ($operator eq '+') {
@@ -115,19 +115,19 @@ sub run_program {
             }
 
             # check for reg depedency
-            my $l_reg = $$instr[0];
-            my $r_reg1 = $$instr[2];
-            my $r_reg2 = $$instr[3];
+            my $l_reg = $instr->{'R1'};
+            my $r_reg1 = $instr->{'R2'};
+            my $r_reg2 = $instr->{'R3'};
 
             my $block = 0;
 
             foreach my $prog_instr (@in_progress) {
-                my $in_progress_l_reg         = $$prog_instr[0];
-                my $in_progress_r_reg1        = $$prog_instr[2];
-                my $in_progress_r_reg2        = $$prog_instr[3];
-                my $in_progress_load_time     = $$prog_instr[4];
-                my $in_progress_d_time        = $$prog_instr[5];
-                my $in_progress_instr_counter = $$prog_instr[6];
+                my $in_progress_l_reg         = $prog_instr->{'R1'};
+                my $in_progress_r_reg1        = $prog_instr->{'R2'};
+                my $in_progress_r_reg2        = $prog_instr->{'R3'};
+                my $in_progress_load_time     = $prog_instr->{'lu_time'};
+                my $in_progress_d_time        = $prog_instr->{'d_time'};
+                my $in_progress_instr_counter = $prog_instr->{'instruction_counter'};
 
                 # read-after-write dependency
                 if ($in_progress_l_reg eq $r_reg1 or $in_progress_l_reg eq $r_reg2) {
@@ -159,25 +159,39 @@ sub run_program {
                 }
                 
             }
-
+            
+            # no dependency, process instruction
             if ($block == 0) {
-                # no dependency, process instruction
+                # Remove from instruction queue
                 shift @instr_queue;
                 $iu_in_use = 1;
 
-                my @progress = @$instr;
+                # Gather process info, and add to process queue
+                my $progress_entry = {
+                    'R1' => $instr->{'R1'},
+                    'Op' => $instr->{'Op'},
+                    'R2' => $instr->{'R2'},
+                    'R3' => $instr->{'R3'},
+                    'lu_time' => $machine->{'lu_time'},
+                    'd_time' => $d_time,
+                    'instruction_counter' => $instruction_counter
+                };
 
-                my $load_time = $machine->{'lu_time'};
-
-                push (@progress, $load_time);
-                push (@progress, $d_time);
-                push (@progress, $instruction_counter);
-
-                my @output_entry = ($instruction_counter, $time_slice, $machine->{'lu_time'}, $d_time, $operator);
-                push(@output, \@output_entry);
+                push(@in_progress, $progress_entry);
+                
+                # Gather info for output, and add to output queue
+                my $output_entry = {
+                    'instruction_counter' => $instruction_counter,
+                    'time_slice' => $time_slice,
+                    'lu_time' => $machine->{'lu_time'},
+                    'd_time' => $d_time,
+                    'Op' => $operator
+                };
+                
+                push(@output, $output_entry);
+                
+                
                 $instruction_counter++;
-
-                push(@in_progress, \@progress);
             }
         }
 
@@ -190,31 +204,30 @@ sub run_program {
         # process in-progress
         my @to_be_removed;
         for (my $j = 0; $j < scalar @in_progress; $j++) {
-            my @cur_instr = @{$in_progress[$j]};
+            my $cur_instr = $in_progress[$j];
             
-            #print "Instruction: " . $cur_instr[6] . " d_units: " . $d_in_use . " d_time: " . $cur_instr[5] . " time slice: " . $time_slice . "\n";
             # load_time
-            if ($cur_instr[4] > 0) {
-                $cur_instr[4]--;
-                if ($cur_instr[4] eq 0) {
+            if ($cur_instr->{'lu_time'} > 0) {
+                $cur_instr->{'lu_time'}--;
+                if ($cur_instr->{'lu_time'} eq 0) {
                     $iu_freed = 1;
-                    $cur_instr[4] = -1; # indicates that d_time should start
+                    $cur_instr->{'lu_time'} = -1; # indicates that d_time should start
                 }
             }
             # d_time start
-            elsif ($cur_instr[4] eq -1) {
+            elsif ($cur_instr->{'lu_time'} eq -1) {
                 if ($d_in_use < $machine->{'d_units'}) {
                     $d_in_use++;
-                    $cur_instr[5]--;
+                    $cur_instr->{'d_time'}--;
 
-                    $cur_instr[4] = 0; # set flag back to normal
+                    $cur_instr->{'lu_time'} = 0; # set flag back to normal
                     
                     my $d_start_time = $time_slice;
                     my $found_entry = 0;
                     for ( my $output_entry = 0; $output_entry < scalar (@output); $output_entry++) {
                         # find current instruction to add d_start_time
-                        if (${$output[$output_entry]}[0] eq $cur_instr[6]) {
-                            push (@{$output[$output_entry]}, $d_start_time);
+                        if ($output[$output_entry]->{'instruction_counter'} eq $cur_instr->{'instruction_counter'}) {
+                            $output[$output_entry]->{'d_start_time'} = $d_start_time;
                             $found_entry = 1;
                             last;
                         }
@@ -222,19 +235,19 @@ sub run_program {
                 }
                 # no d-units available
                 else {
-                    my @dependency_entry = ($cur_instr[6], $time_slice, "D", -1);
+                    my @dependency_entry = ($cur_instr->{'instruction_counter'}, $time_slice, "D", -1);
                     push (@dependency_blocks, \@dependency_entry);
                 }
             }
             # d_time
             else {
-                $cur_instr[5]--;
+                $cur_instr->{'d_time'}--;
             }
 
-            $in_progress[$j] = \@cur_instr;
+            $in_progress[$j] = $cur_instr;
 
             # instruction finished
-            if ($cur_instr[5] <= 0) {
+            if ($cur_instr->{'d_time'} <= 0) {
                 $d_freed++;
 
                 push(@to_be_removed, $j);
@@ -272,12 +285,12 @@ sub display_HP {
     my $col_max = 0;
 
     foreach my $instr (@output) {
-        my $instr_number = $$instr[0];
-        my $time_slice   = $$instr[1];
-        my $lu_time      = $$instr[2];
-        my $d_time       = $$instr[3];
-        my $operator     = $$instr[4];
-        my $d_start_time = $$instr[5];
+        my $instr_number = $instr->{'instruction_counter'};
+        my $time_slice   = $instr->{'time_slice'};
+        my $lu_time      = $instr->{'lu_time'};
+        my $d_time       = $instr->{'d_time'};
+        my $operator     = $instr->{'Op'};
+        my $d_start_time = $instr->{'d_start_time'};
 
         my $col_counter = 0;
         my $line = "S" . $instr_number . ":   ";
@@ -304,11 +317,11 @@ sub display_HP {
             }
         }
 
-        $line = $line . " $operator |";
+        $line = $line . " ${operator} |";
         $col_counter++;
 
         for (my $k = $d_time-1; $k > 0; $k--) {
-            $line = $line . " $operator |";
+            $line = $line . " ${operator} |";
             $col_counter++;
         }
 
